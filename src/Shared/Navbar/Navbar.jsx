455fec09f7dfc3179e57be6fd74252c8853efd32
programming hero <naofelbadsha@gmail.com>
2024-09-28 10:44:08 +0100

    added navber

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,44 +1,106 @@
+import { Link } from "react-router-dom";
+import Container from "../Container/Container";
+import useAuth from "../../Hooks/useAuth";
+import { useState } from "react";
+import avatarImg from '../../assets/images/placeholder.jpg'
+// import avatarImg from '../../../assets/images/placeholder.jpg'
+import { AiOutlineMenu } from "react-icons/ai";
+
+
+
 const Navbar = () => {
+  const {user, logOut} = useAuth();
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <div>
-      <div className="navbar bg-base-100">
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-5 w-5 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
-            </svg>
-          </button>
-        </div>
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-5 w-5 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-              ></path>
-            </svg>
-          </button>
-        </div>
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex flex-row  items-center justify-between gap-3 md:gap-0">
+            {/* Logo */}
+            <Link to="/">
+              <img
+                // className='hidden md:block'
+                src="https://i.ibb.co/4ZXzmq5/logo.png"
+                alt="logo"
+                width="100"
+                height="100"
+              />
+            </Link>
+            {/* Dropdown Menu */}
+            <div className="relative">
+              <div className="flex flex-row items-center gap-3">
+                {/* Become A Host btn */}
+                <div className="hidden md:block">
+                  {!user && (
+                    <button
+                      disabled={!user}
+                      className="disabled:cursor-not-allowed cursor-pointer hover:bg-neutral-100 py-3 px-4 text-sm font-semibold rounded-full  transition"
+                    >
+                      Host your home
+                    </button>
+                  )}
+                </div>
+                {/* Dropdown btn */}
+                <div
+                  onClick={() => setIsOpen(!isOpen)}
+                  className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
+                >
+                  <AiOutlineMenu />
+                  <div className="hidden md:block">
+                    {/* Avatar */}
+                    <img
+                      className="rounded-full"
+                      referrerPolicy="no-referrer"
+                      src={user && user.photoURL ? user.photoURL : avatarImg}
+                      alt="profile"
+                      height="30"
+                      width="30"
+                    />
+                  </div>
+                </div>
+              </div>
+              {isOpen && (
+                <div className="absolute rounded-xl shadow-md w-[40vw] md:w-[10vw] bg-white overflow-hidden right-0 top-12 text-sm">
+                  <div className="flex flex-col cursor-pointer">
+                    <Link
+                      to="/"
+                      className="block md:hidden px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+                    >
+                      Home
+                    </Link>
+
+                    {user ? (
+                      <>
+                        <div
+                          onClick={logOut}
+                          className="px-4 py-3 hover:bg-neutral-100 transition font-semibold cursor-pointer"
+                        >
+                          Logout
+                        </div>
+                      </>
+                    ) : (
+                      <>
+                        <Link
+                          to="/login"
+                          className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+                        >
+                          Login
+                        </Link>
+                        <Link
+                          to="/signup"
+                          className="px-4 py-3 hover:bg-neutral-100 transition font-semibold"
+                        >
+                          Sign Up
+                        </Link>
+                      </>
+                    )}
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </Container>
       </div>
     </div>
   );
