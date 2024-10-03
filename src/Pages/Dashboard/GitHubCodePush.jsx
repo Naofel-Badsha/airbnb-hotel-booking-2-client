d4bd4dd211ce0947977d9e7f2ae6410db759fe8c
programming hero <naofelbadsha@gmail.com>
2024-10-03 05:19:34 +0100

    added Dctor card item

---
+++ src/Pages/Dashboard/GitHubCodePush.jsx
@@ -0,0 +1,106 @@
+const GitHubCodePush = () => {
+  return (
+    <div>
+      <div className="gird gap-5 md:grid-cols-2 lg:grid-cols-3">
+        {/*---------------Card-------1---------*/}
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co.com/8gXzThG/doctor-6.png"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        {/*---------------Card-------2---------*/}
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co.com/VM7b52F/doctor-5.png"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        {/*---------------Card-------3---------*/}
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co.com/KwXCkmM/doctor-4.png"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        {/*---------------Card-------4---------*/}
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co.com/M937w5K/doctor-3.png"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        {/*---------------Card-------5---------*/}
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co.com/thsjFGF/doctor-1.png"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        {/*---------------Card-------6---------*/}
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co.com/5cKRjcz/doctor-2.png"
+              alt="Shoes"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GitHubCodePush;
